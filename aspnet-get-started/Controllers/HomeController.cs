5dd599f2856dc9f21468920d0ed85c73fa718a7f
scrambler89 <118729556+scrambler89@users.noreply.github.com>
2022-11-21 15:30:52 +0100

    home controller page update

--- aspnet-get-started/Controllers/HomeController.cs
+++ aspnet-get-started/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace aspnet_get_started.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact page.";
 
             return View();
         }

74946a56930a3e446d14f9237253eaf706313eb8
scrambler89 <118729556+scrambler89@users.noreply.github.com>
2022-11-22 23:23:00 +0100

    Rolledback the changes

--- aspnet-get-started/Controllers/HomeController.cs
+++ aspnet-get-started/Controllers/HomeController.cs
@@ -7,32 +7,8 @@ using System.Web.Mvc;
 namespace aspnet_get_started.Controllers
 {
     public class HomeController : Controller
-    {
-        public ActionResult Index()
         {
-            public class Datum
-                {
-                    public int id { get; set; }
-                    public string email { get; set; }
-                    public string first_name { get; set; }
-                    public string last_name { get; set; }
-                    public string avatar { get; set; }
-                }
-            public class Root
-                {
-                    public int page { get; set; }
-                    public int per_page { get; set; }
-                    public int total { get; set; }
-                    public int total_pages { get; set; }
-                    public List<Datum> data { get; set; }
-                    public Support support { get; set; }
-                }
-            public class Support
-                {
-                    public string url { get; set; }
-                    public string text { get; set; }
-                }
-            return View();
+             return View();
         }
 
         public ActionResult About()

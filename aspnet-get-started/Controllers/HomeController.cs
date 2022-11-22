8823ab8b4b70d89929be001160ff097bba7a8723
scrambler89 <118729556+scrambler89@users.noreply.github.com>
2022-11-22 23:17:06 +0100

    updated cs with class files

--- aspnet-get-started/Controllers/HomeController.cs
+++ aspnet-get-started/Controllers/HomeController.cs
@@ -10,10 +10,29 @@ namespace aspnet_get_started.Controllers
     {
         public ActionResult Index()
         {
-            var json = "{"id": "7","email": "michael.lawson@reqres.in","first_name": "Michael","last_name": "Lawson",
-            "avatar": "https://reqres.in/img/faces/7-image.jpg"}"
-            JSONModel.Rootobject records = JsonConvert.DeserializeObject<JSONModel.Rootobject>(json); 
-            return View(records);
+            public class Datum
+                {
+                    public int id { get; set; }
+                    public string email { get; set; }
+                    public string first_name { get; set; }
+                    public string last_name { get; set; }
+                    public string avatar { get; set; }
+                }
+            public class Root
+                {
+                    public int page { get; set; }
+                    public int per_page { get; set; }
+                    public int total { get; set; }
+                    public int total_pages { get; set; }
+                    public List<Datum> data { get; set; }
+                    public Support support { get; set; }
+                }
+            public class Support
+                {
+                    public string url { get; set; }
+                    public string text { get; set; }
+                }
+            return View();
         }
 
         public ActionResult About()
